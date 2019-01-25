cd4def3419520e8eb92619029f3de0bec9348ee9
asumandang <32838966+asumandang@users.noreply.github.com>
2019-01-25 18:07:04 +0800

    Model files

---
+++ HotSeat.java
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * @author arriane.artiaga
+ *
+ */
+public class HotSeat extends Seat{
+	
+	/**
+	 * Increases the price to be paid by 20% of the regular price
+	 * @param price - the seat price
+	 * @param code - the seat code
+	 */
+	public HotSeat(float price, String code){
+		super((float)(price+price*.2), code);
+	}
+}
