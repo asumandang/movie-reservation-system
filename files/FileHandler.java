5d42831ad453fbe6002fb33170c69d5d8ac6906a
asumandang <32838966+asumandang@users.noreply.github.com>
2019-01-30 06:39:40 +0800

    Add files via upload

---
+++ files/FileHandler.java
@@ -0,0 +1,13 @@
+package files;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FileHandler {
+	
+	public HashMap<String, ArrayList<Seat>> getReservedSeats(int scheduleId){
+		
+	}
+	
+	public void deleteReservation(int reservationId){}
+}
